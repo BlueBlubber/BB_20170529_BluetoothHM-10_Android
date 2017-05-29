3962121123a69905154c3ee5aed7eabf6b10dfc9
BlueBlubber <benjamin.birnbach@siemens-ppal.com>
2017-05-29 19:42:31 +0200

    .ino added
    Arduino file added

---
+++ BB_20170529_BluetoothHM-10_Android.ino
@@ -0,0 +1,34 @@
+#include <SoftwareSerial.h>
+
+#define ledPin 13
+#define rxPin 10
+#define txPin 11
+
+SoftwareSerial btSerial(rxPin, txPin);
+String btData;
+String btData1;
+
+void setup() {
+  btSerial.begin(9600);
+  Serial.begin(9600);
+  btSerial.println("bluetooth available");
+  Serial.println("bluetooth available");
+  pinMode(ledPin, OUTPUT);
+}
+void loop() {
+  if (btSerial.available() || Serial.available()) {
+    btData1 = Serial.readString();
+    btData = btSerial.readString();
+    if (btData == "on" || btData1 == "on") {
+      digitalWrite(ledPin, 1);
+      btSerial.println("LED on Pin 13 is on_Bluetooth");
+      Serial.println("LED on Pin 13 is on_Serial");
+    }
+    if (btData == "off" || btData1 == "off") {
+      digitalWrite(ledPin, 0);
+      btSerial.println("LED on Pin 13 is off_Bluetooth");
+      Serial.println("LED on Pin 13 is off_Serial");
+    }
+  }
+  delay(100);
+}
